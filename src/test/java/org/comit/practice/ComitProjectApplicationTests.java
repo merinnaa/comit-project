5c6740e2c051fc4feba1559ded2df078a77291ec
LUWAM YOHANNES <luli@3c:06:30:55:c9:e7.hitronhub.home>
2022-08-28 22:46:53 -0600

    Add all files

---
+++ src/test/java/org/comit/practice/ComitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.comit.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
